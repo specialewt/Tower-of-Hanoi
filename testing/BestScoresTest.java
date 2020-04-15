0b093b04be5137a891bb2b267a3d8abd6a58afa2
Lauren E. Schmiedeler <schmiedelerle@hopper.slu.edu>
2020-04-15 10:51:10 -0500

    updating LevelTest and BestScoresTest

--- testing/BestScoresTest.java
+++ testing/BestScoresTest.java
@@ -1,8 +1,8 @@
-package testing;
+package model;
 
-import model.*;
 import java.io.File;
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class BestScoresTest {
--- testing/LevelTest.java
+++ testing/LevelTest.java
@@ -1,7 +1,7 @@
-package testing;
+package model;
 
-import model.*;
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class LevelTest {
@@ -70,5 +70,77 @@ public class LevelTest {
     	Level level = new Level(1);
     	boolean result = level.isEnd();
     	assertEquals("isEnd()", false, result);
+    }
+    
+    @Test
+    public void testGetLevelExpectTrue()
+    {
+    	Level level = new Level(1);
+    	boolean result = (level.getLevel() == 1);
+    	assertEquals("getLevel()", true, result);
+    }
+    
+    @Test
+    public void testGetMaxRingsExpectTrue()
+    {
+    	Level level = new Level(1);
+    	boolean result = (level.getMaxRings() == 3);
+    	assertEquals("getMaxRings()", true, result);
+    }
+    
+    @Test
+    public void testGetMinMovesExpectTrue()
+    {
+    	Level level = new Level(1);
+    	boolean result = level.getMinMoves() == 7;
+    	assertEquals("getMinMoves()", true, result);
+    }
+    
+    @Test
+    public void testGetMoveCounterExpectTrue()
+    {
+    	Level level = new Level(1);
+    	boolean result = level.getMoveCounter() == 0;
+    	assertEquals("getMoveCounter()", true, result);
+    }
+    
+    @Test
+    public void testGetMoveCounterExpectTrue2()
+    {
+    	Level level = new Level(1);
+    	level.move(1, 0); // invalid move should not increase move counter
+    	boolean result = level.getMoveCounter() == 0;
+    	assertEquals("getMoveCounter()", true, result);
+    }
+    
+    @Test
+    public void testGetMoveCounterExpectTrue3()
+    {
+    	Level level = new Level(1);
+    	level.move(0, 1); // valid move should increase move counter
+    	boolean result = level.getMoveCounter() == 1;
+    	assertEquals("getMoveCounter()", true, result);
+    }
+    
+    @Test
+    public void testGetPostExpectTrue()
+    {
+    	Level level = new Level(1);
+    	Post post = new Post();
+    	post.add(new Ring(3));
+    	post.add(new Ring(2));
+    	post.add(new Ring(1));
+    	System.out.println(post);
+    	boolean result = level.getPost(0).toString().equals(post.toString());
+    	assertEquals("getPost()", true, result);
+    }
+    
+    @Test
+    public void testGetPostExpectTrue2()
+    {
+    	Level level = new Level(1);
+    	Post post = new Post();
+    	boolean result = level.getPost(1).toString().equals(post.toString());
+    	assertEquals("getPost()", true, result);
     }
 }
