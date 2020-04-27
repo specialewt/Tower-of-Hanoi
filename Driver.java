75d9d195339b7740f89ea90cb31c212b239ad4d3
Will S <william.speciale@slu.edu>
2020-04-27 12:36:51 -0400

    driver updates

--- Driver.java
+++ Driver.java
@@ -1,31 +1,18 @@
 import model.*;
+import view.*;
 import controller.*;
 
-import java.util.Scanner;
 
-public class Driver {
+public class Driver
+{
+    public static void main(String []args)
+    {
+        GameFrame localGF = new GameFrame();
+        HomePanel homeScreen = new HomePanel(localGF.getFrameWidth(),localGF.getFrameHeight());
+        BestScores newBestScores = new BestScores(5,"model/.TOHSaveFile.txt");
+        MainController gameController = new MainController(homeScreen,localGF, newBestScores);
 
-    public static void main(String args[]){
-
-        TerminalController game = new TerminalController();
-        System.out.println(game);
-
-        while(true) {
-            game.play();
-
-            Scanner s = new Scanner(System.in);
-            System.out.println("Choose from the following:\n (1) Play again\n (2) Change Level\n (3) Quit");
-
-            int choice = Integer.parseInt(s.nextLine());
-            if (choice == 1) {
-                game.restartLevel();
-                System.out.println(game);
-            } else if (choice == 2) {
-                game.selectLevel();
-                System.out.println(game);
-            } else if (choice == 3) {
-                System.exit(0);
-            }
-        }
+        gameController.startGame();
     }
+
 }
